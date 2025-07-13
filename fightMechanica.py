854d20c0c69f7030478f706b1a7e3b3c251a2d85
fzzytronics <pabloguz72562@gmail.com>
2025-07-12 19:18:53 -0500

    Adds fight mechanics base classes
    Adds initial classes for implementing fight mechanics.
    
    These classes (Fight and Opp) provide a foundation for defining fights and opponents,
    including basic attributes like HP, attacks, and rewards.
    
    The code also integrates with existing game systems and handles potential
    import errors gracefully.

--- fightMechanica.py
+++ fightMechanica.py
@@ -1,6 +1,50 @@
-import math
-import random
-import time
-import Termcolor
+# I stole this from an undertale python clone, will be adapting it to use for our fights
 
+class Fight:
+    def __init__(self):
+        """
+        Describes a Fight.
+        """
+        self.id = 0
+        self.opponents = []
+        self.rewards = []
 
+    def opps_except(self, opp: Opp) -> [type]:
+        """
+        Get opponents in this Fight except the given one.
+        """
+        tmp = self.opponents.copy()
+        try:
+            tmp.remove(opp)
+        except ValueError:
+            pass
+        tmp = [type(i) for i in tmp]
+        return tmp
+
+    def update_opps(self) -> None:
+        """
+        For each opponent in the Fight, tell them that they are a part of this Fight.
+        """
+        for i in self.opponents:
+            i.fight = self
+
+class Opp:
+    def __init__(self, fight: Fight):
+        """
+        Describes an opponent.
+        :param fight: the Fight that this opponent is a part of.
+        """
+        self.id = 0
+        self.hp = 1
+        self.max_hp = 1
+        self.alive = True
+        self.fight = fight
+        self.attacks = []
+        self.name = ''
+        self.gold_on_spare = 0
+        self.gold_on_kill = 0
+        self.exp_on_kill = 0
+
+"""
+TOOD: define an attack and an enemy attack
+"""
---
+++ fightsToPlay/haroldFight.py
@@ -0,0 +1,5 @@
+"""
+Following the trend, this will be a minigame, will have to make a victory/defeat 
+"""
+
+import pygame
