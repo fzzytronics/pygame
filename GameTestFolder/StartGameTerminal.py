4682914df376245ec630a879d60fd0c7db2fd058
JaedynM <mcneiljaedyn87@gmail.com>
2025-07-24 14:23:05 -0500

    Prototype for Jayson
    run it pls

---
+++ GameTestFolder/StartGameTerminal.py
@@ -0,0 +1,9 @@
+import time
+import new_pygame
+print("Welcome...")
+time.sleep(1)
+print("This is the terminal for the main game.")
+print("Loading...")
+time.sleep(4)
+print("DONE!")
+new_pygame.game()
